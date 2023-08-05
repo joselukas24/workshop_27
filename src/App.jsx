ea8eb0d56ac8c6c4f194d5aca6e46de6f8ecbe46
Jose Lopez Renteria <joselukas24@gmail.com>
2023-08-04 23:56:10 -0500

    done with the form

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import SignupForm from "./components/SignupForm";
 import Authenticate from "./components/Authenticate";
+import { useState } from "react";
 
 function App() {
   return (
--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -1,3 +1,41 @@
+import { useState } from "react";
+
 export default function SignupForm() {
-  return <h2>Sign Up</h2>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    console.log("Success");
+  }
+
+  return (
+    <>
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:{" "}
+          <input
+            type="text"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Password:{" "}
+          <input
+            type="text"
+            value={password}
+            onChange={(event) => {
+              setPassword(event.target.value);
+            }}
+          />
+        </label>
+        <br />
+        <button type="submit">Sumbit</button>
+      </form>
+    </>
+  );
 }
